efe5235df8d70bbf1c2b4a1934f082e661fb9f0c
Andyferfl <andviolin7@gmail.com>
2025-02-03 01:21:58 +0300

    Changed allowed hosts

--- api/task/settings.py
+++ api/task/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=e%hj@1wqfd*((st+khclg%l#0a6&4n+p&_1yr87c#p-^tv_u6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['andyferfl.duckdns.org','127.0.0.1','localhost']
 
 
 # Application definition
