13242b682cb67d59b0d8585b402075fff88e7f8d
Andyferfl <andviolin7@gmail.com>
2025-02-03 00:09:45 +0300

    Added domain to allowed hosts

--- api/task/settings.py
+++ api/task/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=e%hj@1wqfd*((st+khclg%l#0a6&4n+p&_1yr87c#p-^tv_u6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['andyferfl.duckdns.org']
 
 
 # Application definition
@@ -162,6 +162,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:3000',
+    'https://andyferfl.duckdns.org'
 ]
 
 SIMPLE_JWT = {
